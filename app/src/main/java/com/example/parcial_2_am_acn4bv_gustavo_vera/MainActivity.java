3abbecfcf3a213e1c0f7da0124ab21099961731b
Gustavo Vera <gusvera.017@gmail.com>
2023-11-26 16:55:07 -0300

    Agregó las dependencias de Firebase según la doc

--- app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/MainActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.parcial_2_am_acn4bv_gustavo_vera;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 }
