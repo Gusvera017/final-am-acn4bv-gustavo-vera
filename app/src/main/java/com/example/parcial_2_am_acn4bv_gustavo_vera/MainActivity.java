859eb72b4fbb2f44435a36492fd51ce0f5687a8b
Gustavo Vera <gusvera.017@gmail.com>
2023-11-26 18:48:05 -0300

    Compruebo de que Firebase este funcionando

--- app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/MainActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.parcial_2_am_acn4bv_gustavo_vera;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -13,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Log.i("Firebase", "Usuario Autenticado");
+            //reload();
+        } else {
+            Log.i("Firebase", "Usuario NO Autenticado, redirigir");
+        }
     }
 }
