f0ec3af1da6e14ab2f0e590b433d45f68dde1997
Gustavo Vera <gusvera.017@gmail.com>
2023-11-26 20:50:40 -0300

    Obtengo el usuario y contaseña de los inputs

--- app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/LoginActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/LoginActivity.java
@@ -3,9 +3,26 @@ package com.example.parcial_2_am_acn4bv_gustavo_vera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public void login(String email, String password) {
+        Log.i("Firebase", "email: " + email);
+        Log.i("Firebase", "password: " + password);
+    }
+
+    public void onButtonLoginClick (View view) {
+        EditText emailInput = findViewById(R.id.emailInputLogin);
+        EditText passwordInput = findViewById(R.id.passwordInputLogin);
+
+        String emailInputLogin = emailInput.getText().toString();
+        String passwordInputLogin = passwordInput.getText().toString();
+
+        this.login(emailInputLogin, passwordInputLogin);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
