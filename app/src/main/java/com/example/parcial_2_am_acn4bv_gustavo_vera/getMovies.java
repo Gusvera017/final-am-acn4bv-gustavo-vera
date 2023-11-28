775dc8b5d7994253495deb32896462e043dbf753
Gustavo Vera <gusvera.017@gmail.com>
2023-11-28 18:50:06 -0300

    Creo la clase para obtener información de forma asincrona

---
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/getMovies.java
@@ -0,0 +1,11 @@
+package com.example.parcial_2_am_acn4bv_gustavo_vera;
+
+import android.os.AsyncTask;
+
+public class getMovies extends AsyncTask<String, Integer, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
