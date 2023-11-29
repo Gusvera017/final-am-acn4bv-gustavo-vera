821128c3ca6b661cc7d32dfe0ec561e8d0320ead
Gustavo Vera <gusvera.017@gmail.com>
2023-11-29 15:43:54 -0300

    Se agregó Jackson para convertir el JSON a Obj

--- app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/GetMovies.java
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/GetMovies.java
@@ -3,15 +3,39 @@ package com.example.parcial_2_am_acn4bv_gustavo_vera;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Movie {
+    @JsonProperty("title")
+    public String title;
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class MovieData {
+    @JsonProperty("movies")
+    public List<Movie> movies;
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class ApiResponse {
+    @JsonProperty("data")
+    public MovieData data;
+}
 
 public class GetMovies extends AsyncTask<String, Integer, String> {
 
@@ -33,13 +57,17 @@ public class GetMovies extends AsyncTask<String, Integer, String> {
         String response = "";
         try {
             response = run(url);
-            JSONObject moviesInfo = new JSONObject(response);
+            ObjectMapper objectMapper = new ObjectMapper();
+            ApiResponse apiResponse = objectMapper.readValue(response, ApiResponse.class);
+            response = apiResponse.data.movies.get(0).title;
+            /*JSONObject moviesInfo = new JSONObject(response);
             JSONObject data = moviesInfo.getJSONObject("data");
             JSONArray movies = (JSONArray) data.get("movies");
             JSONObject moviesInfo0 = (JSONObject) movies.get(0);
             String nameMovie0 = (String) moviesInfo0.get("title");
-            response = nameMovie0;
-        } catch (IOException | JSONException e) {
+            response = nameMovie0;*/
+        } catch (IOException e/*| JSONException e*/) {
+            Log.e("Error", "Error en la conversión del JSON", e);
             throw new RuntimeException(e);
         }
         return response;
