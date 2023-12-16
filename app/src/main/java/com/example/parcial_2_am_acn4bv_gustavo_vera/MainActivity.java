7a6893fe35df1d26b282ebe1cea0ed2519e02a96
Gustavo Vera <gusvera.017@gmail.com>
2023-12-16 14:30:10 -0300

    Obtengo los datos del usuario de firestore

--- app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/MainActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_gustavo_vera/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.parcial_2_am_acn4bv_gustavo_vera;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,12 +11,28 @@ import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    /* ORIGINAL CODE
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        checkConnection();
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         checkConnection();
     }
 
@@ -44,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /* ORIGINAL CODE
+
     private void checkUserAuthentication() {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
@@ -54,6 +74,33 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
         }
-    }
+    } */
 
+    private void checkUserAuthentication() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            db.collection("users")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()) {
+                                for(QueryDocumentSnapshot document : task.getResult()) {
+                                    String id = document.getId();
+                                    Object data = document.getData();
+                                    Log.i("firestore", "id: " + id + " data: " + data.toString());
+                                }
+                            }
+                        }
+                    });
+
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+    }
 }
